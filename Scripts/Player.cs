47e39a858a8b1bcd0509247afeba1b16db153c4a
karabylka <dutov.vl@gmail.com>
2021-01-19 13:59:40 +1000

    rotation 360 fix

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -39,7 +39,34 @@ namespace Toil.Scripts
 
         private void RotateForward(){
             var dir = sprite.GetDirection();
-            sprite.Rotate((float)Math.Atan2(dir.Y, dir.X));
+            var rot = (float)Math.Atan2(dir.Y, dir.X);
+            sprite.Rotate(CurveAngle(sprite.rotation, rot, 0.06f));
+        }
+
+        //MOVE IT TO LIB
+        private float CurveAngle(float from, float to, float step)
+        {
+            if (step == 0) return from;
+            if (from == to || step == 1) return to;
+
+            Vector2 fromVector = new Vector2((float)Math.Cos(from), (float)Math.Sin(from));
+            Vector2 toVector = new Vector2((float)Math.Cos(to), (float)Math.Sin(to));
+
+            Vector2 currentVector = Slerp(fromVector, toVector, step);
+
+            return (float)Math.Atan2(currentVector.Y, currentVector.X);
+        }
+
+        private Vector2 Slerp(Vector2 from, Vector2 to, float step)
+        {
+            if (step == 0) return from;
+            if (from == to || step == 1) return to;
+
+            double theta = Math.Acos(Vector2.Dot(from, to));
+            if (theta == 0) return to;
+
+            double sinTheta = Math.Sin(theta);
+            return (float)(Math.Sin((1 - step) * theta) / sinTheta) * from + (float)(Math.Sin(step * theta) / sinTheta) * to;
         }
     }
 }
