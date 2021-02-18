a22ad32a6a311e75ac781e99e3828cdc0eb77772
karabylka <dutov.vl@gmail.com>
2021-02-18 18:34:02 +1000

    Day push

--- Scripts/ECS/Systems/TrailSystem.cs
+++ Scripts/ECS/Systems/TrailSystem.cs
@@ -28,21 +28,26 @@ namespace Gabiac.Scripts.ECS.Systems
         private void Update(in Transform _transform, ref Trail _trail){
             int currentTrailLength = Math.Clamp((int)_transform.GetDeltaPosition(), 0, _trail.MaxPointsCount);
             if(_trail.TrailPoints.Count < currentTrailLength){
-                _trail.TrailPoints.AddLast();
+                //_trail.TrailPoints.AddLast();
             }
         }
 
-        private void PoolOut(LinkedList<TrailPoint> _list, List<TrailPoint> _pool){
-            if(_pool.Count > 0){
-                int lastElementIndex = _pool.Count-1;
-                TrailPoint pooledElem = _pool[lastElementIndex];
-                _pool.RemoveAt(lastElementIndex);
-                _list.AddLast(pooledElem);
-            }
+        private void PoolOut(LinkedList<TrailPoint> _list, TrailPoint[] _pool){
+            // if(_pool.Count > 0){
+            //     int lastElementIndex = _pool.Count-1;
+            //     TrailPoint pooledElem = _pool[lastElementIndex];
+            //     _pool.RemoveAt(lastElementIndex);
+            //     _list.AddLast(pooledElem);
+            // }
         }
 
         private void PoolIn(LinkedList<TrailPoint> _list, List<TrailPoint> _pool){
-
+            // if(_pool.Count < _pool.){
+            //     int lastElementIndex = _pool.Count-1;
+            //     TrailPoint pooledElem = _pool[lastElementIndex];
+            //     _pool.RemoveAt(lastElementIndex);
+            //     _list.AddLast(pooledElem);
+            // }
         }
 
         protected override void PostUpdate(float _state) => world.Optimize(runner, spriteBatch.End);
